c0db14c610135a9d63681bf49745bac74011ce30
Nguyen Huu Trinh <trinhsp89@gmail.com>
2018-11-07 03:08:48 +0000

    Update utils/common.py

--- utils/common.py
+++ utils/common.py
@@ -1,31 +1,55 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import os
+
+# as usual 
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# cross-validation split
 from sklearn.utils import class_weight, shuffle
 
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
-
-import os
+# image preprocessing
 import PIL
 from PIL import Image
 import cv2
 from imgaug import augmenters as iaa
-from tqdm import tqdm
-import warnings
-warnings.filterwarnings("ignore")
 
-root_path = "/home/trinhnh1/Documents/train_data/kaggle/human-protein/input"
+# keras modules
+import keras
+from keras import backend as K
+from keras.preprocessing import image
 from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential, load_model
+from keras.models import Sequential, Model, load_model
 from keras.layers import Activation, Dropout, Flatten, Dense, GlobalMaxPooling2D, BatchNormalization, Input, Conv2D
 from keras.applications.inception_v3 import InceptionV3
 from keras.callbacks import ModelCheckpoint
 from keras import metrics
 from keras.optimizers import Adam 
-from keras import backend as K
-import keras
-from keras.models import Model
+from keras.models import 
 import keras.losses
 # create callbacks list
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler, EarlyStopping, ReduceLROnPlateau
 from sklearn.model_selection import train_test_split
+
+# processbar and datetime
+from tqdm import tqdm
+import datetime as dt
+
+# slack nofify
+from slackclient import SlackClient
+
+# limit GPU when training data
+import tensorflow as tf
+from keras import backend as K
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.75
+sess = tf.Session(config=config)
+K.set_session(sess)
+
+# warning ignore
+import warnings
+warnings.filterwarnings("ignore")
+
+root_path = "/home/trinhnh1/Documents/train_data/kaggle/human-protein/input"
+start = dt.datetime.now()
